0be503ac5c57963634024f2ebd5bf22d93035a1c
Sofroniebruh <esnu@MacBook-Air-Sofronie.local>
2025-01-12 10:56:58 +0100

    added dependency

--- src/main/java/com/example/Netflix/CORS/WebConfig.java
+++ src/main/java/com/example/Netflix/CORS/WebConfig.java
@@ -1,10 +1,12 @@
 package com.example.Netflix.CORS;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -26,6 +28,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         WebMvcConfigurer.super.configureMessageConverters(converters);
         converters.add(new MappingJackson2HttpMessageConverter());
-        converters.add(new Jaxb2RootElementHttpMessageConverter());  // For XML
+        converters.add(new Jaxb2RootElementHttpMessageConverter());
     }
 }
